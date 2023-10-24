1cc6fd401737b9d1e95334edd24d647b37b09d95
ladyboy <cabralclara77@gmail.com>
2023-10-24 18:08:13 -0300

    24/10

--- javascript.js
+++ javascript.js
@@ -14,7 +14,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "11/27/",
+      dayMonth = "11/26/",
       birthday = dayMonth + yyyy;
   
     today = mm + "/" + dd + "/" + yyyy;
